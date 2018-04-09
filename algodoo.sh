fcd7776241f4fd786f9f72b14a857fdc9da73d68
MatMercer <mateusmercer@gmail.com>
2018-04-09 00:00:51 -0300

    Remove unused .algodoo folder from home

--- algodoo.sh
+++ algodoo.sh
@@ -2,24 +2,23 @@
 
 progName="algodoo"
 progRealPath="/usr/share/$progName/app"
-progHome="$HOME/.$progName"
 progBin="Algodoo.exe"
 
-unset WINEPREFIX
-if [ ! -d $progHome ] ; then
-   mkdir -p $progHome
-
-   # User folders
-   find $progRealPath -maxdepth 1 -type d -exec cp -r {} $progHome/ \;
-
-   # User configs
-   find $progRealPath -maxdepth 1 -type f -name "*.cfg" -exec cp {} $progHome/ \;
-
-   # Read only files
-   ln -s $progRealPath/*.{txt,dll,exe} $progHome
-fi
+# unset WINEPREFIX
+# if [ ! -d $progHome ] ; then
+#    mkdir -p $progHome
+# 
+#    # User folders
+#    find $progRealPath -maxdepth 1 -type d -exec cp -r {} $progHome/ \;
+# 
+#    # User configs
+#    find $progRealPath -maxdepth 1 -type f -name "*.cfg" -exec cp {} $progHome/ \;
+# 
+#    # Read only files
+#    ln -s $progRealPath/*.{txt,dll,exe} $progHome
+# fi
 
 # msvcp90 MUST be overriden https://www.reddit.com/r/linuxquestions/comments/6g077y/wine_error_class_not_registered/dqn9c8k/
 export WINEDLLOVERRIDES="msvcp90=n"
-wine $progHome/$progBin "$@"
+wine $progRealPath/$progBin "$@"
 
